9574fe9055080668d44e0720bba08e3c84cdd846
nimaxnimax <avaroti.service@gmail.com>
2024-01-30 13:35:53 +0100

    done

---
+++ azure_virtual_machine/Lab_Test_02/commands.sh
@@ -0,0 +1,12 @@
+# Create a resource group in azure**
+
+# Initialize Terraform
+terraform init
+
+# Create a Terraform execution plan
+terraform plan -out main.tfplan
+
+# Apply a Terraform execution plan
+terraform apply main.tfplan
+
+# terraform destroy

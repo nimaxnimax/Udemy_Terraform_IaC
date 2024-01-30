52929fed4b1e2eaf1849e67c657e3248e900f82a
nimaxnimax <avaroti.service@gmail.com>
2024-01-30 13:33:04 +0100

    done

--- azure_virtual_machine/Lab_Test_01/azure_cli_terraform_install_windows.ps1
+++ azure_virtual_machine/Lab_Test_01/azure_cli_terraform_install_windows.ps1
@@ -1,20 +1,21 @@
-# Installation_AzureCLI_Terraform
+# Install Terraform on the local computer
 
-#Azure CLI
+# Azure CLI
 $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; Remove-Item .\AzureCLI.msi
 
-#Chocolatey
+# Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-#Terraform
+# Terraform
 choco install -y terraform
 
-#PowerShell Core
+# PowerShell Core
 winget install --id Microsoft.Powershell --source winget
 
-#Visual Studio Code
+# Visual Studio Code
 winget install -e --id Microsoft.VisualStudioCode
 
+# Login
 az config set core.allow_broker=true
 az account clear
 az login
